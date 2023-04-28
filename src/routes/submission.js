a69bddbd62d718480db0ac7817e66e069f377c7c
Anurag Ravi <kumarayush1014@gmail.com>
2023-04-28 13:09:39 +0530

    added assignment submission flow

--- src/routes/submission.js
+++ src/routes/submission.js
@@ -4,21 +4,25 @@ const router = express.Router();
 const Assignment = require('../models/assignment');
 const Submission = require('../models/submission');
 const multer = require('multer');
+const { userMiddleware } = require('../middlewares/user');
 const storage = multer.diskStorage({
     destination: async function (req, file, cb) {
         const { submission_id } = req.params;
         const submission = await Submission.findById(submission_id);
         if (!submission) {
+            req.submitted = false;
             cb(null,'junk');
         } else {
             const assignment = await Assignment.findById(submission.assignment);
             const up = "files/" + assignment.course + "/" + assignment._id + "/" + submission._id;
+            console.log(up, typeof(up));
             submission.file = {
                 mimetype : file.mimetype,
                 filename : file.originalname,
                 path : up
             }
             await submission.save();
+            req.submitted = true;
             cb(null, up);
         }
     },
@@ -29,9 +33,10 @@ const storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-router.post('/:assignment_id', submissionController.createSubmission);
-router.post('/add/:submission_id', upload.single('file'), submissionController.submitFile);
+router.post('/:assignment_id', userMiddleware ,submissionController.createSubmission);
+router.post('/add/:submission_id',userMiddleware , upload.single('file'), submissionController.submitFile);
 router.get('/file/:submission_id', submissionController.getSubmissionFile);
+router.get('/check/:assignment_id',userMiddleware , submissionController.checkSubmission);
 
 module.exports = router;
             
